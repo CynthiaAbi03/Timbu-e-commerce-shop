c4c7654e6ef9e2ac1e8e390dd5251638e9c0c76b
CynthiaAbi03 <119008657+CynthiaAbi03@users.noreply.github.com>
2024-07-15 07:06:30 +0100

    handling filter sidebar rendering

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -27,7 +27,7 @@ const Product = () => {
     <div>
       <Header />
 
-      <div className="w-full max-w-[2200px] mx-auto pr-custom-mr pl-custom-ml mt-[44px] gap-[3.5rem]  flex flex-col max-sm:pl-[24px] max-sm:pr-[24px] max-md:px-[32px]">
+      <div className="w-full max-w-[2200px] mx-auto pr-custom-mr pl-custom-ml mt-[44px] gap-[3.5rem]  flex flex-col max-sm:pl-[24px] max-sm:pr-[24px] max-md:mt-[180px] max-sm:gap-[3rem] max-sm:mt-[180px] max-md:px-[32px]">
         <div className="flex items-center gap-[.5rem]">
           <Link
             className="w-[48px] h-[48px] max-sm:w-[40px] max-sm:h-[40px] rounded-full bg-none flex items-center justify-center hover:bg-gray-200 transition"
@@ -41,21 +41,23 @@ const Product = () => {
           </Link>
           <p className="text-[1.5rem] max-sm:text-[1.5rem]">Back</p>
         </div>
-        {filteredProducts.map((item, index) => {
-          return (
-            <ProductDetails
-              key={index}
-              id={item.id}
-              name={item.name}
-              image1={item.image1}
-              image2={item.image2}
-              price={item.price}
-              description={item.description}
-              category={item.category}
-              type={item.type}
-            />
-          );
-        })}
+        
+          {filteredProducts.map((item, index) => {
+            return (
+              <ProductDetails
+                key={index}
+                id={item.id}
+                name={item.name}
+                image1={item.image1}
+                image2={item.image2}
+                price={item.price}
+                description={item.description}
+                category={item.category}
+                type={item.type}
+              />
+            );
+          })}
+        
       </div>
       <Footer />
     </div>
